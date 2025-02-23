89f6a1eae0eabf953c458bbbdac18e2354d45d29
PARK GEONDO <116621655+GD000908@users.noreply.github.com>
2025-02-23 21:53:47 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		int num_1=0;
+		int num_2=0;
+		Scanner sc = new Scanner(System.in);
+		num_1=sc.nextInt();
+		num_2=sc.nextInt();
+		System.out.print(num_1+num_2);
+	}
+}
