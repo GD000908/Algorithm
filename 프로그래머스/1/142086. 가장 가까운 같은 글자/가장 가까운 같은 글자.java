a332c1aba66cc90630dcc549038159b077a0fdd9
PARK GEONDO <116621655+GD000908@users.noreply.github.com>
2025-08-29 13:51:07 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 14.63 ms, Memory: 92.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        Arrays.fill(answer, -1);
+
+        for (int i = 1; i < s.length(); i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    answer[i] = i - j; // 가장 가까운 거리
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
